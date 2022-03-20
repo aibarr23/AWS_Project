a6157ade5cffc6ce40161cc469e823005e81988d
Anthony Ibarra <aibarr23@uic.edu>
2022-03-20 13:51:36 -0500

    finishing last commitment

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ def SendW_toCtr(T, Tfl, Pre):
     address = ('10.1.15.243', 5000) #define server IP and port
     client_socket = socket.socket(socket.AF_INET, SOCK_DGRAM) #set op the socket
     client_socket.settimeout(1) # Only wait 1 second for a response
-    data = "WData:" + ";" + T + ";" + Tfl + ";" + Pre# ";" + Ap + ";" + H + ";" + Wd + ";" + Ws + + ";" + Cp
+    data = "WData" + ";" + T + ";" + Tfl + ";" + Pre# ";" + Ap + ";" + H + ";" + Wd + ";" + Ws + + ";" + Cp
 
     while(1):
         
@@ -19,8 +19,8 @@ def SendW_toCtr(T, Tfl, Pre):
         try:
 
             rec_data, addr = client_socket.recvfrom(2048) # read response from arduino
-            temp = float(rec_data) #Convert string rec_data to float temp
-            print ("The Measured Temperature is ", temp, "degreeF.")#print the result
+            res = float(rec_data) #Convert string rec_data to float res(response)
+            print (res)#print the result
         except:
             pass
         
