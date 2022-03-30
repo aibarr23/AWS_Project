fbf927996a51d649f08e53c085f7a12beec31eaf
JBiscanUIC <77802942+JBiscanUIC@users.noreply.github.com>
2022-03-29 21:23:23 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,55 +1,325 @@
-#from socket import *
-import socket
-import time
-from datetime import datetime
 
-address = ('127.0.0.1', 5000) #define server IP and port
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #set op the socket
-client_socket.settimeout(1) # Only wait 1 second for a response
-   
-# this will send the information to the arduino(Controller) via UDP cient
-# function variables format=> (Temperature);(Feels Like(temp));(Air pressure);(Humidity);(Wind direction);(Wind speed);(cloud percentage);(Precipitation(of the recent or near hour))
-# Currently only sending most needed Temperature, temp feals like, and precipitation
-def SendW_toCtr(T, Tfl, Pre):
+
+KV = '''
+#:import get_color_from_hex kivy.utils.get_color_from_hex
+
+
+MDBoxLayout:
+    orientation: "vertical"
+
+    MDToolbar:
+        md_bg_color: get_color_from_hex("#000a12")
+        color_normal: get_color_from_hex("#718089")
+        color_active: get_color_from_hex("#f3ab44")
+        title: "Automated Watering APP"
+        elevation: 10
+        right_action_items: [["brightness-4", app.theme_style]]
+
     
-    data = "WData" + ";" + T + ";" + Tfl + ";" + Pre# ";" + Ap + ";" + H + ";" + Wd + ";" + Ws + + ";" + Cp
-
-    while(1):
-        
-        client_socket.sendto(data.encode(), address)
-
-        try:
-
-            rec_data, addr = client_socket.recvfrom(2048) # read response from arduino
-            #res = float(rec_data) #Convert string rec_data to float res(response)
-            print("Received response from:"+ addr)
-            print (rec_data)#print the result
-        except:
-            print("Request timed out, could not send")
-            break
-            #pass
-        
-        time.sleep(2) #delay befor sending next command
+    MDTabs:
+        text_color_active: get_color_from_hex("#f3ab44")
+        tab_hint_x: True
+        id: tabs
         
 
+        Weather_tab:
+            id: weathertab
+            icon: "weather-sunny"
+            color_active: get_color_from_hex("#f3ab44")
 
-# this loop will check for possible recieved packets
-def store_Data(self):
-    
-    try:
-        R_data, addr = client_socket.recvfrom(1024) # read response
-        time_recv = datetime.now()
-        
-        # get data and store it in text file for future analysis
-        if(R_data[0].decode().size() > 0):
             
-            data_R = R_data[0].decode()
-            data = time_recv + ": " + data_R + "\n"
+
+            MDBoxLayout:
+                orientation: "vertical"
+                
+
+                MDToolbar:
+                    id: weatherheading
+                    elevation: 10
+                    halign: "center"
+
+                    title: app.Current_City
+                    pos_hint: {"center_y": .5, "pos_hint_x": 0.5}
+                    font_style: "H4"
+
+                    
+                    right_action_items: [["refresh", app.refresh_weather, "Refresh"]]
+                    
+                    MDFlatButton:
+                        pos_hint: {"center_y": .5, "pos_hint_x":0.5}
+                        text: "auto-refresh"
+                        text_color: get_color_from_hex("#f3ab44")
+                        padding: [0,0,0,0]
+                    MDSwitch:
+                        id: AR
+                        pos_hint: {"center_y": .5, "pos_hint_x":0.5}
+                        tooltip_text: "auto-refresh"
+                
+                MDStackLayout:
+                    ScrollView:
+                        id: scrview
+                        pos_hint: {'center_x': .5}
+                        adaptive_size: True
+                        
+                        MDList:
+                            id: list
+                            halign: "center"
+                            cols: 1
+                            rows: 5
+                            spacing: "40dp"
+                            
+                            OneLineListItem:
+                                id: datetime
+                                text: app.getDT
+                            
+                                
+                            MDCard:
+                                orientation: "vertical"
+                                size_hint: .5, None
+                                height: "300dp"
+
+                                padding: 30, 30, 30, 30
+                                bg_light:
+                                elevation: 20
+                                radius: 40
+
+                                MDLabel:
+                                    halign: "center"
+                                    font_style: "H5"
+                                    text: "Weather for Today"
+
+                                MDLabel:
+                                    id: cd1line1
+                                    halign: "center"
+                                    font_style: "H1"
+                                    text: app.CTemp
+
+                                MDSeparator:
+
+                                MDLabel:
+                                    id: cd1line2
+                                    halign: "center"
+                                    font_style: "H4"
+                                    text: app.Tdata
+                                    
+
+                            MDCard:
+                                size_hint: .5, None
+                                height: "300dp"
+
+                                padding: 30, 30, 30, 30
+                                bg_light:
+                                elevation: 20
+                                radius: 40
+
+
+                                MDLabel:
+                                    id: mdc2
+                                    halign: "center"
+                                    font_style: "H3"
+                                    text: app.Wdata
+
+                            MDCard:
+                                size_hint: .5, None
+                                height: "300dp"
+
+                                padding: 30, 30, 30, 30
+                                bg_light:
+                                elevation: 20
+                                radius: 40
+
+
+                                MDLabel:
+                                    id: mdc3
+                                    halign: "center"
+                                    font_style: "H4"
+                                    text: app.Adata
             
-            #send data to app to update here
+
+
+
+
+
+
+
+        system_tab:
+            icon: "wrench"
+
+            MDBoxLayout:
+                orientation: "vertical"
+                # md_bg_color: get_color_from_hex("#344954")
+                color_normal: get_color_from_hex("#718089")
+                color_active: get_color_from_hex("#f3ab44")
             
-            text_file = open("data.txt", "a")
-            text_file.write(data)
-            text_file.close()
-    except:
-        pass
+
+                MDToolbar:
+                    elevation: 10
+                    halign: "center"
+
+                    title: "System Status"
+                    pos_hint: {"center_y": .5, "pos_hint_x": 0.5}
+                    font_style: "H4"
+
+                MDStackLayout:
+
+                    ScrollView:
+                        pos_hint: {'center_x': .5}
+                        adaptive_size: True
+                        
+                        MDLabel:
+                            text: "Solenoid Status"
+                            halign: "center"
+                            font_style: "H4"
+                            size_hint_y: None
+                            height: self.texture_size[1]
+                            pos_hint: {"center_y": .5}
+                        MDList:
+                            halign: "center"
+                            cols: 2
+                            rows: 5
+                            spacing: "40dp"
+
+                
+
+                            
+                            MDLabel:
+                                text: "Single-line item"
+                            MDSwitch:
+                                id: AR
+                                pos_hint: {"center_y": .5, "pos_hint_x":5}
+                            
+                            MDLabel:
+                                text: "Solenoid #2"
+                            MDSwitch:
+                                id: AR
+                                pos_hint: {"center_y": .5, "pos_hint_x":5}
+                            MDLabel:
+                                text: "Solenoid #3"
+                            MDSwitch:
+                                id: AR
+                                pos_hint: {"center_y": .5, "pos_hint_x":5}
+
+
+
+
+
+
+
+
+
+        plant_tab:
+            icon: "flower"
+
+            MDBoxLayout:
+                orientation: "vertical"
+                # md_bg_color: get_color_from_hex("#344954")
+                color_normal: get_color_from_hex("#718089")
+                color_active: get_color_from_hex("#f3ab44")
+            
+
+                MDToolbar:
+                    elevation: 10
+                    halign: "center"
+
+                    title: "Plant Information"
+                    pos_hint: {"center_y": .5, "pos_hint_x": 0.5}
+                    font_style: "H4"
+
+                MDStackLayout:
+
+                    ScrollView:
+                        pos_hint: {'center_x': .5}
+                        adaptive_size: True
+                        
+                        MDList:
+                            halign: "center"
+                            cols: 1
+                            rows: 5
+                            spacing: "40dp"
+
+                            #start of first plant
+                            MDCard:
+                                orientation: "vertical"
+                                size_hint: .5, None
+                                height: "300dp"
+                       
+                                padding: 30, 30, 30, 30
+                                bg_light:
+                                elevation: 20
+                                radius: 40
+
+                                MDLabel:
+                                    halign: "center"
+                                    font_style: "H3"
+                                    text: "Plectranthus Venteri"
+
+                                MDLabel:
+                                    halign: "center"
+                                    font_style: "H5"
+                                    text: "Distinctive deeply lobed pelted leaves on self branching stout stems; the purplish mauve blossoms usually appear in late fall. Perfect where you need a tough as nails plant which gives an appearance of lacy delicacy"
+
+                            
+                            MDGridLayout:
+                                cols: 1
+                                row_default_height: (self.width - self.cols*self.spacing[0]) / self.cols
+                                row_force_default: True
+                                adaptive_height: True
+                                padding: dp(4), dp(4)
+                                spacing: dp(4)
+
+                                SmartTileWithLabel:
+                                    source: "plant1.jpg"
+                                    text: "[size=26]--Plant Name-- 1[/size]"
+                                    allow_stretch: True
+                                    keep_ratio: True
+
+                                                  
+                            MDCard:
+                                orientation: "vertical"
+                                size_hint: .5, None
+                                height: "300dp"
+
+                                padding: 30, 30, 30, 30
+                                bg_light:
+                                elevation: 20
+                                radius: 40
+
+                                MDLabel:
+                                    halign: "center"
+                                    font_style: "H5"
+                                    text: "Current Soil Moisture of Plant #1: "
+
+                                MDLabel:
+                                    halign: "center"
+                                    font_style: "H5"
+                                    text: "Plant #1 Watered by Solenoid --Solenoid number here-- "
+
+                                MDSeparator:
+
+                                MDLabel:
+                                    halign: "center"
+                                    font_style: "H5"
+                                    text: "Current Soil Moisture of Plant #2: "
+
+                                MDLabel:
+                                    halign: "center"
+                                    font_style: "H5"
+                                    text: "Plant #2 Watered by Solenoid --Solenoid number here-- "
+
+                                MDSeparator:
+
+                                MDLabel:
+                                    halign: "center"
+                                    font_style: "H5"
+                                    text: "Current Soil Moisture of Plant #3: "
+
+                                MDLabel:
+                                    halign: "center"
+                                    font_style: "H5"
+                                    text: "Plant #3 Watered by Solenoid --Solenoid number here-- "
+                        
+                        
+
+
+
+'''
