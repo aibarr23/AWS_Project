1a410a81359ea564c67557dbe027c2d9c1eac872
JBiscanUIC <77802942+JBiscanUIC@users.noreply.github.com>
2022-04-08 16:13:26 -0500

    Update adjusted_watering_code.ino

--- adjusted_watering_code.ino
+++ adjusted_watering_code.ino
@@ -1,5 +1,6 @@
 
 #include <Arduino.h>
+#include <stdbool.h>
 // Attach the serial display's RX line to digital pin 2
 Uart mySerial (&sercom0, 13, 8, SERCOM_RX_PAD_1, UART_TX_PAD_0);
 
@@ -26,6 +27,15 @@ int soilPin2 = A2;
 int soilPower = 7;
 int soilPower1 = 8;
 int soilPower2 = 9;  
+
+boolean solenoidControl0 == False; // user is not controlling solenoid0
+boolean solenoidControl1 == False; // user is not controlling solenoid1
+boolean solenoidControl2 == False; // user is not controlling solenoid2
+boolean solenoidControlOpen0 == False; // solenoid0 is not open
+boolean solenoidControlOpen1 == False; // solenoid1 is not open
+boolean solenoidControlOpen2 == False; // solenoid2 is not open
+boolean returnControl == False; // microcontroller is currently in control of solenoids
+boolean closeAll == False; // closing all solenoids is currently false
 
 void setup(){
   mySerial.begin(9600); // set up serial port for 9600 baud (speed)
@@ -58,7 +68,7 @@ void setup(){
   pinMode(solenoidPin1, OUTPUT);//Set D11 as an OUTPUT
   digitalWrite(solenoidPin1, LOW);//Set to LOW so no power is flowing through the sensor
 
-    mySerial.begin(9600); // set up serial port for 9600 baud (speed)
+  mySerial.begin(9600); // set up serial port for 9600 baud (speed)
   delay(500); // wait for display to boot up
 
   pinMode(solenoidPin2, OUTPUT);//Set D12 as an OUTPUT
@@ -76,9 +86,26 @@ void loop() {
 
   int sensorValue2;
   sensorValue2 = readSoil2();
+  
+  if (returnControl == True) { //user returns control to solenoids
+  solenoidControl0 = False; // user is not controlling solenoid0
+  solenoidControl1 = False; // user is not controlling solenoid1   
+  solenoidControl2 = False; // user is not controlling solenoid2
+  }
+  
+  if (solenoidControl0 == False) {
+    int wateringPot(sensorValue, solenoidPin);
+  }
+  if (solenoidControl1 == False) {
+    int wateringPot(sensorValue1, solenoidPin1);
+  }
+  if (solenoidControl2 == False) {
+    int wateringPot(sensorValue2, solenoidPin2);
+  }
+}
 
-
-  // POT ONE 
+int wateringPot (int sensorvalue, int solenoidPin) { //what do i refer to high/low as?
+    
   if (sensorValue <= thresholdDown) {
     //water it
     digitalWrite(solenoidPin, HIGH); //open solenoid
@@ -102,15 +129,15 @@ void loop() {
       digitalWrite(solenoidPin, HIGH); //open solenoid
       delay(61000); //wait a minute and a second 
       sensorValue = readSoil();
-      if (sensorValue <= thresholdDown) {
-        delay(60000); //wait a minute
-        digitalWrite(solenoidPin, LOW); //close solenoid
-        delay(1000);
-      }
-      else {
-        digitalWrite(solenoidPin, LOW); //close solenoid
-        delay(1000);
-      }
+    if (sensorValue <= thresholdDown) {
+      delay(60000); //wait a minute
+      digitalWrite(solenoidPin, LOW); //close solenoid
+      delay(1000);
+    }
+    else {
+      digitalWrite(solenoidPin, LOW); //close solenoid
+      delay(1000);
+    }
     }
   }
   else {
@@ -120,126 +147,38 @@ void loop() {
       digitalWrite(solenoidPin, HIGH); //open solenoid
       delay(61000); //wait a minute and a second 
       sensorValue = readSoil();
-      if (sensorValue <= thresholdDown) {
-        delay(60000); //wait a minute
-        digitalWrite(solenoidPin, LOW); //close solenoid
-        delay(1000);
-      }
-      else {
-        digitalWrite(solenoidPin, LOW); //close solenoid
-        delay(1000);
-      }
-    }
-  }
-
-  //POT TWO 
-    if (sensorValue1 <= thresholdDown) {
-    //water it
-    digitalWrite(solenoidPin1, HIGH); //open solenoid
-    delay(61000); //wait a minute and a second 
-    sensorValue1 = readSoil1();
-    if (sensorValue1 <= thresholdDown) {
+    if (sensorValue <= thresholdDown) {
       delay(60000); //wait a minute
-      digitalWrite(solenoidPin1, LOW); //close solenoid
+      digitalWrite(solenoidPin, LOW); //close solenoid
       delay(1000);
     }
-    else {
-      digitalWrite(solenoidPin1, LOW); //close solenoid
-      delay(1000);
-    }
-  }
-  else if (sensorValue1 >= thresholdUp) {
-    //wait and check later 
-    delay(36000000); //wait an hour
-    sensorValue1 = readSoil1();
-    if (sensorValue1 <= thresholdDown) {
-      digitalWrite(solenoidPin1, HIGH); //open solenoid
-    delay(61000); //wait a minute and a second 
-    sensorValue1 = readSoil1();
-    if (sensorValue1 <= thresholdDown) {
-      delay(60000); //wait a minute
-      digitalWrite(solenoidPin1, LOW); //close solenoid
-      delay(1000);
-    }
-    else {
-      digitalWrite(solenoidPin1, LOW); //close solenoid
-      delay(1000);
-    }
-  }
-  }
   else {
-    delay(18000000); //wait 30 min 
-    sensorValue1 = readSoil1();
-    if (sensorValue1 <= thresholdDown) {
-      digitalWrite(solenoidPin1, HIGH); //open solenoid
-    delay(61000); //wait a minute and a second 
-    sensorValue1 = readSoil1();
-    if (sensorValue1 <= thresholdDown) {
-      delay(60000); //wait a minute
-      digitalWrite(solenoidPin1, LOW); //close solenoid
-      delay(1000);
-    }
-    else {
-      digitalWrite(solenoidPin1, LOW); //close solenoid
-      delay(1000);
-    }
+    digitalWrite(solenoidPin, LOW); //close solenoid
+    delay(1000);
   }
-  }
-
-  //POT THREE
-    if (sensorValue2 <= thresholdDown) {
-    //water it
-    digitalWrite(solenoidPin2, HIGH); //open solenoid
-    delay(61000); //wait a minute and a second 
-    sensorValue2 = readSoil2();
-    if (sensorValue2 <= thresholdDown) {
-      delay(60000); //wait a minute
-      digitalWrite(solenoidPin2, LOW); //close solenoid
-      delay(1000);
     }
-    else {
-      digitalWrite(solenoidPin2, LOW); //close solenoid
-      delay(1000);
-    }
-  }
-  else if (sensorValue2 >= thresholdUp) {
-    //wait and check later 
-    delay(36000000); //wait an hour
-    sensorValue2 = readSoil2();
-    if (sensorValue2 <= thresholdDown) {
-      digitalWrite(solenoidPin2, HIGH); //open solenoid
-    delay(61000); //wait a minute and a second 
-    sensorValue2 = readSoil2();
-    if (sensorValue2 <= thresholdDown) {
-      delay(60000); //wait a minute
-      digitalWrite(solenoidPin2, LOW); //close solenoid
-      delay(1000);
-    }
-    else {
-      digitalWrite(solenoidPin2, LOW); //close solenoid
-      delay(1000);
-    }
-  }
-  }
-  else {
-    delay(18000000); //wait 30 min 
-    sensorValue2 = readSoil2();
-    if (sensorValue2 <= thresholdDown) {
-      digitalWrite(solenoidPin2, HIGH); //open solenoid
-    delay(61000); //wait a minute and a second 
-    sensorValue2 = readSoil2();
-    if (sensorValue2 <= thresholdDown) {
-      delay(60000); //wait a minute
-      digitalWrite(solenoidPin2, LOW); //close solenoid
-      delay(1000);
-    }
-    else {
-      digitalWrite(solenoidPin2, LOW); //close solenoid
-      delay(1000);
-    }
-  }
   }
 }
+
+int solenoids(bool solenoidControl, bool solenoidControlopenclose) {
+  if (solenoidControl == True) { //user interacts with solenoid0 switch
+    if (solenoidControlopenclose == True) { //user opens solenoid
+      digitalWrite(solenoidPin, HIGH);
+    }
+    else if (solenoidcontrol == False){ //close solenoid when it is open
+      digitalWrite(solenoidPin, LOW);
+    }
+    else {
+      continue;
+    }
+  }
+  
+  if (closeAll == True) { //closing all solenoids, microcontrller is still not in control
+    digitalWrite(solenoidPin, HIGH);
+    digitalWrite(solenoidPin1, HIGH);
+    digitalWrite(solenoidPin2, HIGH);
+  }
+}  
 
 int readSoil()
 {
