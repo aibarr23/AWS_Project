3928e67a9afbb71198fc494d3f00e79c657145c3
JBiscanUIC <77802942+JBiscanUIC@users.noreply.github.com>
2022-04-12 19:50:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,137 @@ Author 2: Joe Biscan
 Date: Spring 2022
 
 '''
+import socket
+import time
+from datetime import datetime
+
+address = ('10.0.0.56', 2390) #define server IP and port
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #set op the socket
+client_socket.settimeout(1) # Only wait 1 second for a response
+
+# this will send the information to the arduino(Controller) via UDP cient
+# function variables format=> (Temperature);(Feels Like(temp));(Air pressure);(Humidity);(Wind direction);(Wind speed);(cloud percentage);(Precipitation(of the recent or near hour))
+# Currently only sending most needed Temperature, temp feals like, and precipitation
+def SendW_toCtr(T, Tfl, Pre):
+    
+    data = "WData" + ";" + T + ";" + Tfl + ";" + Pre# ";" + Ap + ";" + H + ";" + Wd + ";" + Ws + + ";" + Cp
+
+    while(1):
+        
+        client_socket.sendto(data.encode(), address)
+
+        try:
+            
+            rec_data, addr = client_socket.recvfrom(2048) # read response from arduino
+            #res = float(rec_data) #Convert string rec_data to float res(response)
+            print("Received response from:"+ addr)
+            print (rec_data)#print the result
+        except:
+            print("Request timed out, could not send")
+            break
+            #pass
+        
+        time.sleep(2) #delay befor sending next command
+
+
+
+# this will send notify the controller that it has control again
+def Return_control():
+    data = "Rcntr;"# Rcntr stands for Return control
+    while(1):
+        client_socket.sendto(data.encode(), address)
+
+        try:
+
+            rec_data, addr = client_socket.recvfrom(2048) # read response from arduino
+            #res = float(rec_data) #Convert string rec_data to float res(response)
+            print("Received response from:"+ addr)
+            print (rec_data)#print the result
+            x = "accepted"
+            return rec_data.decode(), str(x)
+        except:
+            print("Request timed out, could not send")
+            return "rejected", "denide"
+        
+
+
+
+
+# this will close all solenoids
+# so: stands for solenoid-> 0: all solenoids; 1: solenoid 1, etc.
+# action: c: close; o: open
+def CLASo(so, action):
+    
+    data = "CalSo;"# refers to clos all solenoids
+    
+    while(1):
+        client_socket.sendto(data.encode(), address)
+
+        try:
+
+            rec_data, addr = client_socket.recvfrom(2048) # read response from arduino
+            #res = float(rec_data) #Convert string rec_data to float res(response)
+            print("Received response from:"+ addr)
+            print (rec_data)#print the result
+            x = "success"
+            return str(x)
+        except:
+            print("Request timed out, could not send")
+            return "denide"
+
+
+
+# this loop will check for possible recieved packets
+def Update_Data(self):
+    
+    
+    # client_socket.sendto(data.encode(), address)
+    try:
+        R_data, addr = client_socket.recvfrom(1024) # read response
+        time_recv = datetime.now()
+        
+        st = R_data.decode()
+        # test store data function
+        print(st)
+        print(time_recv)
+        
+        call_func('main.p', 'Update_UI', st)
+        Update_UI(st)
+            
+        
+        # get data and store it in text file for future analysis
+        S_data(R_data)
+        
+    except:
+        # print("Failed to obtain data")
+        pass
+        
+        
+    
+
+# this is the function that will store the data into a file
+def S_data(R_data):
+    
+    sD = ""
+    time_recv = datetime.now()
+    
+    if(type(R_data) == bytes):
+        sD = str(R_data.decode())
+    else:
+        sD = R_data
+        
+    if(len(sD) > 0):
+            
+        data = time_recv.strftime(" %d/%m/%Y %H:%M:%S\n") + ": " + sD + "\n"
+        
+        #send data to app to update here
+        
+        text_file = open("data.txt", "a")
+        text_file.write(data)
+        text_file.close()
+
+
+
 from kivymd.app import MDApp
 from kivymd.uix.tab import MDTabsBase
 from kivymd.uix.floatlayout import MDFloatLayout
@@ -43,7 +174,7 @@ def Show_Temp_Data(Tfl):
     # Tfl = "-.-"
     # Ttype = "Celsius" #getTempType_FC(desired_type)
 
-    s = "\nFeels like: " + Tfl + "°"  + "  F"
+    s = "\nFeels like: " + Tfl + ""  + "  F"
     return  s
 
 # this will show the data focused on water: for the precipitation, and humidity
@@ -134,7 +265,7 @@ class AWSApp(MDApp):
             Adata = Show_Air_data(formattedmbtoinhg, formattedkmhr, x, clouds)
             getDT = Get_Date_Time()
             T = formattedktof
-            CTemp = "\n" + T + "°" + "  F" + "\n"
+            CTemp = "\n" + T + "" + "  F" + "\n"
 
             self.root.ids.weatherheading.title = Current_City
             self.root.ids.datetime.text = getDT
@@ -293,7 +424,7 @@ class AWSApp(MDApp):
     Adata = Show_Air_data(formattedmbtoinhg, formattedkmhr, x, clouds)
     getDT = Get_Date_Time()
     T = formattedktof
-    CTemp = "\n" + T + "°" + "  F" + "\n"
+    CTemp = "\n" + T + "" + "  F" + "\n"
     
     moS1 = "Current Soil Moisture of Plant #1: "
     moS2 = "Current Soil Moisture of Plant #2: "
